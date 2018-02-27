3bb611c409b751d99d86d4e41cc30dc52dc0765e
Sungjin Han <meinside@gmail.com>
2018-02-27 11:14:19 +0900

    Edit 'unknown command' message

--- main.go
+++ main.go
@@ -205,7 +205,11 @@ func processUpdate(b *bot.Bot, update bot.Update) bool {
 				message = readCode()
 			// fallback
 			default:
-				message = fmt.Sprintf("%s: %s", txt, MessageUnknownCommand)
+				if len(txt) > 0 {
+					message = fmt.Sprintf("%s: %s", txt, MessageUnknownCommand)
+				} else {
+					message = MessageUnknownCommand
+				}
 			}
 		}
 
